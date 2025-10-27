a3750e7225e017c0ec0c72883e5c2e7f9a2c310f
Deichor <deichors@gmail.com>
2025-10-28 01:03:56 +0300

    feat: Remove JUnit dependencies and test task configuration from `cubicolor-exporter` build script

--- cubicolor-exporter/build.gradle.kts
+++ cubicolor-exporter/build.gradle.kts
@@ -16,13 +16,4 @@ dependencies {
 
     // JSON parsing
     implementation("com.google.code.gson:gson:2.10.1")
-
-    // Testing
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
